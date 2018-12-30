14dac0774a315cc1ad6a36cd554f300a2fcfc41d
ant4u <anthonyimontalto@gmail.com>
2018-12-30 02:05:26 -0500

    externalized application data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,9 @@
-const express = require("express");
+const express = require('express');
 const apiRoutes = express.Router();
-const path = require("path");
+const path = require('path');
 
-var userData = [
-  {
-    name: "Stacey",
-    photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
-  },
-  {
-    name: "Jon Snow",
-    photo:
-      "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
-    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
-  }
-];
+// import application data
+let userData = require('../data/friends');
 
 apiRoutes.get("/friends", function(req, res) {
   res.json(userData);
