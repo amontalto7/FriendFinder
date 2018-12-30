12224916889c8df7d6fb5a2f643440db176c9e8e
ant4u <anthonyimontalto@gmail.com>
2018-12-29 23:59:49 -0500

    post functionality works

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,33 +2,33 @@ const express = require("express");
 const apiRoutes = express.Router();
 const path = require("path");
 
-var friendsArr = [
-    {
-      customerName: "Anthony",
-      phoneNumber: "555-777-8888",
-      customerEmail: "email@email.com",
-      customerID: "test2"
-    }
-  ];
+var userData = [
+  {
+    name: "Stacey",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Jon Snow",
+    photo:
+      "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  }
+];
 
 apiRoutes.get("/friends", function(req, res) {
-  res.json(friendsArr);
+  res.json(userData);
 });
 
 apiRoutes.post("/friends", function(req, res) {
-    let newFriend = req.body;
-    console.log(newFriend);
+  let newFriend = req.body;
+  console.log(newFriend);
 
-    // if (reservationArr.length < 5) {
-        friendsArr.push(newFriend);
-        console.log("adding friend")
-    // }
-    // else {
-        // waiting.push(newReservation);
-        // console.log("adding waitlist")
-    // }
-// return, for user validation
-  });
+  userData.push(newFriend);
+  console.log("adding friend");
 
+  // return, for user validation
+});
 
 module.exports = apiRoutes;
