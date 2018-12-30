1a551184eabb7b0e2539846c1b72d2b4d39879fd
ant4u <anthonyimontalto@gmail.com>
2018-12-29 19:39:18 -0500

    landing page created

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -12,11 +12,12 @@ htmlRoutes.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
+// htmlRoutes.use(express.static(__dirname + "../public", {maxAge: 3456700000})); 
+
 // catch all
   htmlRoutes.use(function(req, res){
     // res.sendStatus(404);
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
-
 
 module.exports = htmlRoutes;
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true })); // allows you to process complex objects from the client side
 app.use(express.json());
 
+app.use(express.static('./app/public/assets/images/'));
+
 // Routes
 // =============================================================
 const apiRoutes = require("./app/routing/apiRoutes");
