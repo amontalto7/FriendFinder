5fcaaecb2ddc713411e788ab539e7bb27f0bb434
ant4u <anthonyimontalto@gmail.com>
2018-12-31 14:15:19 -0500

    Finished main logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const apiRoutes = express.Router();
-const path = require('path');
 
 // import application data
 let userData = require('../data/friends');
@@ -13,12 +12,43 @@ apiRoutes.post("/friends", function(req, res) {
   let newFriend = req.body;
   console.log(newFriend);
 
-  userData.push(newFriend);
-  console.log("adding friend");
+    // convert each element to a number
+    newFriend.scores.map(Number); 
 
-    return userData[1];
+    let matchIndex = 0;
+    let lowDiff = 50;
 
-  // return, for user validation
+    const getDiff = function(arr) {
+        let diff = 0;
+
+        for (let j = 0; j < arr.length; j++) {
+            // get difference of each answer, and add it to diff 
+            diff+= Math.abs(arr[j] - newFriend.scores[j]);
+        }
+        return diff;
+    }
+
+  for (let i = 0; i < userData.length; i++) {
+    //   console.log(userData[i].name);
+      let friendScores = userData[i].scores.map(Number);
+    //   console.log(friendScores);
+
+      let totalDiff = getDiff(userData[i].scores);
+        // console.log("Total difference: " + totalDiff);
+      // if this is the best match found so far, update matchIndex and lowDiff vars
+      if (totalDiff < lowDiff) { 
+          lowDiff = totalDiff;
+          matchIndex = i;
+        }
+  }
+
+    // after finding a match, push new user to the friends array
+    userData.push(newFriend);
+    console.log("Adding friend");
+
+    // return the matched user
+    res.send(userData[matchIndex]);
+
 });
 
 module.exports = apiRoutes;
