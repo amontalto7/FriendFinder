aabd370d5fcb0f8d8417954a7da55fc4856c430b
ant4u <anthonyimontalto@gmail.com>
2018-12-31 17:16:07 -0500

    added more data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,51 @@
 let userData = [
     {
-      name: "Stacey Dash",
+      name: "Ned Stark",
       photo:
-        "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-      scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+        "https://d2nzqyyfd6k6c7.cloudfront.net/styles/nova_evo_landscape/s3/article/thumbnail/game_of_thrones_ned_stark_hbo.jpg",
+      scores: ["1", "5", "4", "4", "5", "1", "2", "5", "4", "1"]
     },
     {
       name: "Jon Snow",
       photo:
-        "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
-      scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+        "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/15/09/jon-snow.jpg",
+      scores: ["4", "2", "5", "1", "3", "2", "5", "1", "2", "2"]
     },
     {
-        name: "Hugh Hefner",
+        name: "Sansa Stark",
         photo:
-          "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+          "https://timedotcom.files.wordpress.com/2017/08/game-of-thrones-spoils-od-war-sansa-stark-10.jpg",
         scores: ["3", "4", "2", "5", "5", "1", "2", "4", "3", "4"]
+    },
+    {
+        name: "Daenerys Targaryen",
+        photo:
+          "https://i.pinimg.com/736x/d9/18/47/d91847bfd1e8996581be49c63e74d865.jpg",
+        scores: ["1", "4", "2", "3", "1", "1", "5", "4", "4", "1"]
+    },
+    {
+        name: "Jaime Lannister",
+        photo:
+          "https://upload.wikimedia.org/wikipedia/pt/thumb/0/06/Nikolaj-Coster-Waldau-Game-of-Thrones.jpg/220px-Nikolaj-Coster-Waldau-Game-of-Thrones.jpg",
+        scores: ["2", "3", "4", "3", "2", "1", "3", "4", "", "2"]
+    },
+    {
+        name: "Cersei Cannister",
+        photo:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/9/94/Cersei_Lannister-Lena_Headey.jpg/220px-Cersei_Lannister-Lena_Headey.jpg",
+        scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    },
+    {
+        name: "Petyr Baelish",
+        photo:
+          "https://res.cloudinary.com/jpress/image/fetch/c_fill,f_auto,h_640,q_auto:eco,w_960/https://inews.co.uk/wp-content/uploads/2017/09/Lil-fing.jpg",
+        scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+    },
+    {
+        name: "Missandei",
+        photo:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/5/5d/Missandei-Nathalie_Emmanuel.jpg/220px-Missandei-Nathalie_Emmanuel.jpg",
+        scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
     }
   ];
 
