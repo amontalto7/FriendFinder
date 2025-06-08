9f805b99896d8165e52f7bd7a60b90fc134896c9
Anthony Montalto <37939663+amontalto7@users.noreply.github.com>
2025-06-07 20:43:32 -0400

    re-design, and added images locally

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,49 +2,49 @@ let userData = [
     {
       name: "Ned Stark",
       photo:
-        "https://d2nzqyyfd6k6c7.cloudfront.net/styles/nova_evo_landscape/s3/article/thumbnail/game_of_thrones_ned_stark_hbo.jpg",
+        "..\\assets\\images\\game_of_thrones_ned_stark_hbo.jpg",
       scores: ["1", "5", "4", "4", "5", "1", "2", "5", "4", "1"]
     },
     {
       name: "Jon Snow",
       photo:
-        "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/15/09/jon-snow.jpg",
+        "..\\assets\\images\\jon-snow.jpg",
       scores: ["4", "2", "5", "1", "3", "2", "5", "1", "2", "2"]
     },
     {
         name: "Sansa Stark",
         photo:
-          "https://timedotcom.files.wordpress.com/2017/08/game-of-thrones-spoils-od-war-sansa-stark-10.jpg",
+          "..\\assets\\images\\SophieTurnerasSansaStark.jpg",
         scores: ["3", "4", "2", "5", "5", "1", "2", "4", "3", "4"]
     },
     {
         name: "Daenerys Targaryen",
         photo:
-          "https://i.pinimg.com/736x/d9/18/47/d91847bfd1e8996581be49c63e74d865.jpg",
+          "..\\assets\\images\\d91847bfd1e8996581be49c63e74d865.jpg",
         scores: ["1", "4", "2", "3", "1", "1", "5", "4", "4", "1"]
     },
     {
         name: "Jaime Lannister",
         photo:
-          "https://upload.wikimedia.org/wikipedia/pt/thumb/0/06/Nikolaj-Coster-Waldau-Game-of-Thrones.jpg/220px-Nikolaj-Coster-Waldau-Game-of-Thrones.jpg",
-        scores: ["2", "3", "4", "3", "2", "1", "3", "4", "", "2"]
+          "..\\assets\\images\\Nikolaj-Coster-Waldau-Game-of-Thrones.jpg",
+        scores: ["2", "3", "4", "3", "2", "1", "3", "4", "2", "2"]
     },
     {
         name: "Cersei Lannister",
         photo:
-          "https://upload.wikimedia.org/wikipedia/en/thumb/9/94/Cersei_Lannister-Lena_Headey.jpg/220px-Cersei_Lannister-Lena_Headey.jpg",
+          "..\\assets\\images\\220px-Cersei_Lannister-Lena_Headey.jpg",
         scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
     },
     {
         name: "Petyr Baelish",
         photo:
-          "https://res.cloudinary.com/jpress/image/fetch/c_fill,f_auto,h_640,q_auto:eco,w_960/https://inews.co.uk/wp-content/uploads/2017/09/Lil-fing.jpg",
+          "..\\assets\\images\\Petyr_Baelish.jpg",
         scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
     },
     {
         name: "Missandei",
         photo:
-          "https://upload.wikimedia.org/wikipedia/en/thumb/5/5d/Missandei-Nathalie_Emmanuel.jpg/220px-Missandei-Nathalie_Emmanuel.jpg",
+          "..\\assets\\images\\Missandie_Blue_Dress_Nathalie_Emmanuel.png",
         scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
     }
   ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,54 +1,54 @@
-const express = require('express');
+const express = require("express");
 const apiRoutes = express.Router();
 
 // import application data
-let userData = require('../data/friends');
+let userData = require("../data/friends");
 
-apiRoutes.get("/friends", function(req, res) {
+apiRoutes.get("/friends", function (req, res) {
   res.json(userData);
 });
 
-apiRoutes.post("/friends", function(req, res) {
+apiRoutes.post("/friends", function (req, res) {
   let newFriend = req.body;
   console.log(newFriend);
 
-    // convert each element to a number
-    newFriend.scores.map(Number); 
+  // convert each element to a number
+  newFriend.scores = newFriend.scores.map(Number);
 
-    let matchIndex = 0;
-    let lowDiff = 50;
+  let matchIndex = 0;
+  let lowDiff = 50;
 
-    const getDiff = function(arr) {
-        let diff = 0;
+  const getDiff = function (arr) {
+    let diff = 0;
 
-        for (let j = 0; j < arr.length; j++) {
-            // get difference of each answer, and add it to diff 
-            diff+= Math.abs(arr[j] - newFriend.scores[j]);
-        }
-        return diff;
+    for (let j = 0; j < arr.length; j++) {
+      // get difference of each answer, and add it to diff
+      diff += Math.abs(arr[j] - newFriend.scores[j]);
     }
+    return diff;
+  };
 
   for (let i = 0; i < userData.length; i++) {
     //   console.log(userData[i].name);
-      let friendScores = userData[i].scores.map(Number);
+    let friendScores = userData[i].scores.map(Number);
     //   console.log(friendScores);
 
-      let totalDiff = getDiff(userData[i].scores);
-        // console.log("Total difference: " + totalDiff);
-      // if this is the best match found so far, update matchIndex and lowDiff vars
-      if (totalDiff < lowDiff) { 
-          lowDiff = totalDiff;
-          matchIndex = i;
-        }
+    let totalDiff = getDiff(userData[i].scores);
+    // console.log("Total difference: " + totalDiff);
+    // if this is the best match found so far, update matchIndex and lowDiff vars
+    if (totalDiff < lowDiff) {
+      lowDiff = totalDiff;
+      matchIndex = i;
+    }
   }
 
-    // after finding a match, push new user to the friends array
-    userData.push(newFriend);
-    console.log("Adding friend");
+  // after finding a match, push new user to the friends array
+  userData.push(newFriend);
+  console.log("Adding friend");
 
-    // return the matched user
-    res.send(userData[matchIndex]);
-
+  // return the matched user
+  res.send(userData[matchIndex]);
+  return;
 });
 
 module.exports = apiRoutes;
